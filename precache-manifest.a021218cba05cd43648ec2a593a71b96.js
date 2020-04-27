3873528fb65fca056617d91e0367924f0ba6e39f
cojenco <icathyouyang@gmail.com>
2020-04-26 17:50:42 -0700

    Updates

---
+++ precache-manifest.a021218cba05cd43648ec2a593a71b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9579a5c51a60d1019cb84c0bf0b649ac",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "5b831e817690340195a0",
+    "url": "/tictactoe/static/css/main.daccb090.chunk.css"
+  },
+  {
+    "revision": "c394bb328e8a3da95ca6",
+    "url": "/tictactoe/static/js/2.c518569f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.c518569f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b831e817690340195a0",
+    "url": "/tictactoe/static/js/main.0884bc91.chunk.js"
+  },
+  {
+    "revision": "8f266c157998e9d5700e",
+    "url": "/tictactoe/static/js/runtime-main.2f446db0.js"
+  }
+]);
